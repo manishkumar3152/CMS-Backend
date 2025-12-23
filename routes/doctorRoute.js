21c98864ba8c2ffec74c8490155c69f3df341b05
manishkumar00001 <manish.kumar@arnxt.com>
2025-12-23 11:47:11 +0530

    updated

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -12,8 +12,9 @@ import {
 import authDoctor from "../middlewares/authDoctor.js";
 
 const doctorRouter = express.Router();
+
 doctorRouter.get("/list", doctorList);
-doctorRouter.post("/list", doctorList);
+// doctorRouter.post("/list", doctorList);
 doctorRouter.post("/login", loginDoctor);
 doctorRouter.get("/appointments", authDoctor, appointmentsDoctor);
 doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete);
