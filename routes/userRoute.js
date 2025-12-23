f44b15b46cfd3d1cb64729ea9fef3f2e33d9577f
manishkumar00001 <manish.kumar@arnxt.com>
2025-12-23 11:36:13 +0530

    updated

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,6 +6,7 @@ import uplode from '../middlewares/multer.js'
 
  const userRouter = express.Router()
 
+ userRouter.get('/register',registerUser)
  userRouter.post('/register',registerUser)
  userRouter.post('/login',loginUser)
  userRouter.get('/get-profile',authUser,getProfile)
